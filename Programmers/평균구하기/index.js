9aeea885eb88ae5fceecfba6f7d2404f0711eebb
hanseulhee <3021062@gmail.com>
2021-11-25 23:48:29 +0900

    update 평균구하기

--- Programmers/평균구하기/index.js
+++ Programmers/평균구하기/index.js
@@ -1,6 +1,12 @@
+// 2021.11.25
 const solution = (arr) => {
-  return (
-    arr.reduce((accumulator, currentValue) => accumulator + currentValue) /
-    arr.length
-  );
+  return arr.reduce((a, b) => a + b) / arr.length;
 };
+
+// 2021.08.18
+// const solution = (arr) => {
+//   return (
+//     arr.reduce((accumulator, currentValue) => accumulator + currentValue) /
+//     arr.length
+//   );
+// };
