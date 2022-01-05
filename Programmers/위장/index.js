1c4a7131f736bbba7500c190cf2d0aea56a2f808
hanseulhee <3021062@gmail.com>
2022-01-05 19:33:10 +0900

    위장

--- Programmers/위장/index.js
+++ Programmers/위장/index.js
@@ -1,15 +1,36 @@
+// 2021.12.24
+// function solution(clothes) {
+//   let answer = 1;
+//   let obj = []; // 빈 객체 생성
+
+//   for (let i = 0; i < clothes.length; i++) {
+//     obj[clothes[i][1]] = (obj[clothes[i][1]] || 1) + 1;
+//   }
+//   for (let key in obj) {
+//     answer *= obj[key];
+//   }
+
+//   return answer - 1; // 최소 1가지 이상의 옷을 입기 때문에 -1을 함
+// }
+
+// solution([
+//   ["yellowhat", "headgear"],
+//   ["bluesunglasses", "eyewear"],
+//   ["green_turban", "headgear"],
+// ]);
+
+// 2022.01.05
+
 function solution(clothes) {
   let answer = 1;
-  let obj = []; // 빈 객체 생성
-
+  let arr = [];
   for (let i = 0; i < clothes.length; i++) {
-    obj[clothes[i][1]] = (obj[clothes[i][1]] || 1) + 1;
+    arr[clothes[i][1]] = (arr[clothes[i][1]] || 1) + 1;
   }
-  for (let key in obj) {
-    answer *= obj[key];
+  for (let key in arr) {
+    answer *= arr[key];
   }
-
-  return answer - 1; // 최소 1가지 이상의 옷을 입기 때문에 -1을 함
+  return answer - 1;
 }
 
 solution([
