6783790412078a9cf7f45a0553b1e4d41c0a9736
hanseulhee <3021062@gmail.com>
2023-04-23 15:16:17 +0900

    소문자로바꾸기

---
+++ CodingTest/소문자로바꾸기.js
@@ -0,0 +1,15 @@
+function solution(myString) {
+  console.log(myString.toLowerCase())
+  return myString.toLowerCase()
+}
+
+// function solution(myString) {
+//   let answer = ''
+//   for (let i of myString) {
+//     answer += i.toLowerCase()
+//   }
+//   console.log(answer)
+//   return answer
+// }
+
+solution('aBcDeFg')
