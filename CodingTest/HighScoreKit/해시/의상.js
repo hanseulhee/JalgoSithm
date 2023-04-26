c711a61ea927afd2b07a7aa236f5e9437a9a7289
hanseulhee <3021062@gmail.com>
2023-04-27 00:53:32 +0900

    의상

---
+++ CodingTest/HighScoreKit/해시/의상.js
@@ -0,0 +1,16 @@
+function solution(clothes) {
+  return (
+    Object.values(
+      clothes.reduce((obj, t) => {
+        obj[t[1]] = obj[t[1]] ? obj[t[1]] + 1 : 1
+        return obj
+      }, {}),
+    ).reduce((a, b) => a * (b + 1), 1) - 1
+  )
+}
+
+solution([
+  ['yellow_hat', 'headgear'],
+  ['blue_sunglasses', 'eyewear'],
+  ['green_turban', 'headgear'],
+])
